4bc26d85f3167e3924ed37dde7ce4947f3365494
Adrian Camarena <adriancamarena2310@gmail.com>
2023-11-29 05:38:31 +0100

    video-202 Angular Material - Autocomplete

--- src/app/heroes/pages/searchPage/searchPage.component.ts
+++ src/app/heroes/pages/searchPage/searchPage.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { HeroesService } from '../../services/hero.service';
+import { Hero } from '../../interfaces/hero.interface';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-searchPage',
@@ -7,11 +10,36 @@ import { FormControl } from '@angular/forms';
 })
 export class SearchPageComponent implements OnInit {
 
-  constructor() { }
+  constructor( private heroesService: HeroesService ){}
+
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+public selectedHero? : Hero;
+
+  searchHero() {
+    const value: string = this.searchInput.value || '';
+    this.heroesService.getSuggestions( value )
+    .subscribe( heroes => this.heroes = heroes );
+
+  }
+
+
+  onSelectedOption( event: MatAutocompleteSelectedEvent ): void {
+    if ( !event.option.value ) {
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue( hero.superhero );
+
+    this.selectedHero = hero;
+
+
+  }
 
   ngOnInit() {
   }
 
-  public searchInput = new FormControl('');
 
 }
