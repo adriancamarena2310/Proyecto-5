a2a1da299f8a6b11a34c46be555d6e936c5cf10c
Adrian Camarena <adriancamarena2310@gmail.com>
2023-11-30 01:29:40 +0100

    video-210 HeroForm - ReactiveForms introducción

--- src/app/heroes/pages/newPage/newPage.component.ts
+++ src/app/heroes/pages/newPage/newPage.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-newPage',
   templateUrl: './newPage.component.html'
 })
 export class NewPageComponent implements OnInit {
+
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>( Publisher.DCComics ),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
+
 
   constructor() { }
 
